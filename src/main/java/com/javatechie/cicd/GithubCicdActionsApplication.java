f0caef6b6923070863894c2216357276319f2556
JDK <jacinthdavid70@gmail.com>
2024-07-18 01:48:19 +0100

    - java main commit

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "Welcome to javatechie (BUILT AND DEPLOYED BY JACINTH DAVID)!";
 	}
 
 //	echo "# github-actions-example" >> README.md
